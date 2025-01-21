52a3ae55fce3d9ccbc9c99f9b0c1f14077b0d5ed
yugen <yugen.m7@gmail.com>
2025-01-21 13:03:14 +0545

    minor change

--- main.c
+++ main.c
@@ -8,10 +8,9 @@ static int bigbang(void){
 	return 0;
 }
 
-static void nucleur(void){
+static void warhead(void){
 	printk("boooooooooooooooom, t*e wO*lD iS desTRo*eD");
-	// printk("The world is created");
 }
 
 module_init(bigbang);
-module_exit(nucleur);
+module_exit(warhead);
