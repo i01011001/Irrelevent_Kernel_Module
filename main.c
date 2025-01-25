ed3b76be3757abeb7c05248e01f652b33e3c9866
yugen <yugen.m7@gmail.com>
2025-01-25 22:52:14 +0545

    added doc

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+/**
+ * brief mandatory meta data
+ **/
 MODULE_LICENSE("GPL");
 
+
+/**
+ * @brief function that run when the module is inserted
+ * @return static int
+ **/
 static int main_init(void){
 	printk("A module was created\n");
 	return 0;
 }
 
+/**
+ * @brief  function that run when the module is removed
+ **/
 static void main_exit(void){
 	printk("A modules was destroyed\n");
 }
